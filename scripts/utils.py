dcfd487ffc65ba5c35a3796e84637267211a3e12
Carolina <carolina.cortes@fing.edu.uy>
2025-08-17 23:12:51 +0200

    a

--- scripts/utils.py
+++ scripts/utils.py
@@ -11,7 +11,7 @@ from psycopg2.extras import execute_values
 from dotenv import load_dotenv
 from math import radians, cos, sin, asin, sqrt
 import io
-
+ 
 
 """
     Helper methods for magnitude of change calculation
@@ -358,43 +358,8 @@ def copy_rows(conn, table_name, columns, rows):
         conn.commit()
     except Exception as e:
         conn.rollback()  # reset the transaction
-        bad_rows = []
-        for row in rows:
-            try:
-                with conn.cursor() as cur:
-                    cur.execute(query, row)
-                conn.commit()
-            except Exception as e_row:
-                conn.rollback()
-                bad_rows.append((row, str(e_row)))
 
-                # Try to parse PK columns from the error
-                # Example error: duplicate key value violates unique constraint "entity_pkey"
-                # DETAIL: Key (revision_id, entity_id)=(123, 'abc') already exists.
-                match = re.search(r"Key \((.*?)\)=\((.*?)\)", str(e_row))
-                if match:
-                    key_cols = [col.strip() for col in match.group(1).split(',')]
-                    key_vals = [val.strip() for val in match.group(2).split(',')]
-
-                    # Build WHERE clause dynamically
-                    where_clause = ' AND '.join([f"{col} = %s" for col in key_cols])
-                    select_query = f"SELECT * FROM {table_name} WHERE {where_clause}"
-
-                    try:
-                        with conn.cursor() as cur:
-                            cur.execute(select_query, key_vals)
-                            existing = cur.fetchone()
-                            if existing:
-                                print(f"Existing conflicting row for {dict(zip(key_cols, key_vals))} in table {table_name}: {existing}")
-                                print(f'Row that retunrs error: {e_row}')
-                    except Exception as select_err:
-                        print(f"Error checking for existing row: {select_err}")
-
-        print("\nProblematic rows:")
-        for br, err in bad_rows:
-            print(f"{br} -> {err}")
-
-        print("\nOriginal batch insert error:")
+        print("\Error when doing copy batch error:")
         print(e)
 
 def update_entity_label(conn, entity_id, entity_label):
