aa8dadd0613ee9046061231ab64d4ae52fd10ec8
Carolina <carolina.cortes@fing.edu.uy>
2025-09-15 10:19:59 +0200

    print

--- scripts/page_parser.py
+++ scripts/page_parser.py
@@ -35,6 +35,7 @@ class PageParser():
         self.config = config
 
         print('Initializing PageParser')
+        print(f'Config batch_changes_store: {self.config['batch_changes_store']}')
 
         self.label_hash_counter = 0
         self.description_hash_counter = 0
